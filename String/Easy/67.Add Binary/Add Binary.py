511dc27b1d528fe8773f8f65afdb58848c10d5c6
Jack-Cherish <c411184003@gmail.com>
2017-08-26 13:50:47 +0800

    26 August 2017:Add Binary

---
+++ String/Easy/67.Add Binary/Add Binary.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        if len(a)==0: return b
+        if len(b)==0: return a
+        if a[-1] == '1' and b[-1] == '1':
+            return self.addBinary(self.addBinary(a[0:-1],b[0:-1]),'1') +'0'
+        if a[-1] == '0' and b[-1] == '0':
+            return self.addBinary(a[0:-1],b[0:-1])+'0'
+        else:
+            return self.addBinary(a[0:-1],b[0:-1])+'1'
