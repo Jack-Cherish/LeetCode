1260f284818294b6a0015f25c73fc0e4998aca5e
Jack-Cherish <c411184003@gmail.com>
2017-09-01 10:06:43 +0800

    Longest Common Prefix

---
+++ String/Easy/14.Longest Common Prefix/Longest Common Prefix.py
@@ -0,0 +1,36 @@
+#方法一，利用zip，更快
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+            
+        for i, letter_group in enumerate(zip(*strs)):
+            if len(set(letter_group)) > 1:
+                return strs[0][:i]
+        return min(strs)
+        
+#方法二，利用reduce  
+class Solution:
+
+    def lcp(self, str1, str2):
+        i = 0
+        while (i < len(str1) and i < len(str2)):
+            if str1[i] == str2[i]:
+                i = i+1
+            else:
+                break
+        return str1[:i]
+                                                                                                                                                        
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ''
+        else:
+            return reduce(self.lcp,strs)
---
+++ String/Easy/14.Longest Common Prefix/README.md
@@ -0,0 +1,20 @@
+# 思路
+写一个函数，找到字符串列表中最长公共前缀
+
+### 举例：
+
+	['abcd','abe','ab','ae']
+	
+### 返回：
+	
+	abcd
+	
+### 方法一：
+
+	巧妙利用zip的压缩和set的去重
+	
+### 方法二：
+	
+	利用reduce函数
+	
+	在python3中，reduece函数放到functools包中，需要from functools import reduce进行导入。
