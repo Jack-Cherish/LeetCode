58094b17121847953c4dca5cd5e5d56b5d1f3b2d
Jack-Cherish <c411184003@gmail.com>
2017-09-01 10:17:40 +0800

    Longest Common Prefix

--- String/Easy/14.Longest Common Prefix/Longest Common Prefix.py
+++ String/Easy/14.Longest Common Prefix/Longest Common Prefix.py
@@ -24,6 +24,7 @@ class Solution:
             else:
                 break
         return str1[:i]
+                       
                                                                                                                                                         
     def longestCommonPrefix(self, strs):
         """
